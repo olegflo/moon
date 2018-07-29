b0bda07baf5f089a6192ff5d30c3de11931a1c61
Oleg Soroka <40a.oleg@gmail.com>
2018-07-29 11:28:54 +0200

    Shape default Dockerfile as dev with bound mounts

--- 1.Dockerfile
+++ 1.Dockerfile
@@ -1,7 +1,14 @@
 FROM ruby:2.5-alpine
 
+# throw errors if Gemfile has been modified since Gemfile.lock
+RUN bundle config --global frozen 1
+
+
+WORKDIR /usr/src/app
+
+RUN apk add bash
+
 RUN apk add --update \
-        bash         \
         build-base   \
         libxml2-dev  \
         libxslt-dev  \
@@ -9,11 +16,12 @@ RUN apk add --update \
         nodejs       \
         tzdata
 
-WORKDIR /usr/src/app
 
 COPY Gemfile Gemfile.lock ./
 
 RUN bundle install
+
+COPY . .
 
 EXPOSE 3001
 
--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,8 +1,8 @@
 ActiveAdmin.register_page "Dashboard" do
 
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+  menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
+  content title: proc { I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
         span I18n.t("active_admin.dashboard_welcome.welcome")
@@ -12,16 +12,17 @@ ActiveAdmin.register_page "Dashboard" do
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel "Recent Products" do
+          ul do
+            Product.all.map do |product|
+              li link_to(product.title, admin_product_path(product))
+            end
+          end
+        end
+      end
+    end
 
     #   column do
     #     panel "Info" do
